432c19c66da660635898380ec08ca2a738252ed0
Git-Nivrak <59925169+Git-Nivrak@users.noreply.github.com>
2020-08-28 20:51:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -24,8 +24,8 @@ function update() {
 function incrs() {
 if (money > cost) { 
 	money.minus(cost);
-	increment = Decimal(1).plus(increment.pow(1.3));
-	cost = Decimal(10) + increment.pow(1.2);
+	increment = Decimal(1).plus(increment.pow(1.3)).round;
+	cost = Decimal(10).plus(increment.pow(1.2));
 }
 
 }
