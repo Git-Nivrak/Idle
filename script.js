2184b1e9b34c1c7bd7170efc2617c7be66f865c9
Git-Nivrak <59925169+Git-Nivrak@users.noreply.github.com>
2020-08-28 18:58:39 +0300

    f

---
+++ script.js
@@ -0,0 +1,40 @@
+
+function game() {
+var money = 0;
+var increment = 1;
+var cost = 10;
+
+function pad(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+function func(){
+	money += increment/1000;
+	setTimeout(func,1);
+}
+
+function update() {
+	document.getElementById("mn").innerHTML = Math.round(money);
+	document.getElementById("sp").innerHTML = increment;
+	document.getElementById("cs").innerHTML = cost;
+	setTimeout(update,10);
+}
+
+function incrs() {
+if (money > cost) { 
+	money -= cost;
+	increment += 1 + increments ** 1.3;
+	cost = 10 + Math.round(increment ** 1.2)
+}
+if (increment => 100) { 
+	document.body.innerHTML = "Bruh you're bored";
+}
+
+}
+
+func();
+update();
+}
+game();
