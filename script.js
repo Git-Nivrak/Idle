fbcb1e27c5126eb91a00ad6591f837b9a2cee882
Git-Nivrak <59925169+Git-Nivrak@users.noreply.github.com>
2020-08-28 19:02:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 
-var money = 0;
-var increment = 1;
-var cost = 10;
+var mn = 0;
+var inc = 1;
+var cst = 10;
 
 function pad(num, size) {
     var s = num+"";
@@ -11,24 +11,24 @@ function pad(num, size) {
 }
 
 function func(){
-	money += increment/1000;
+	mn += inc/1000;
 	setTimeout(func,1);
 }
 
 function update() {
-	document.getElementById("mn").innerHTML = Math.round(money);
-	document.getElementById("sp").innerHTML = increment;
-	document.getElementById("cs").innerHTML = cost;
+	document.getElementById("mn").innerHTML = Math.round(mn);
+	document.getElementById("sp").innerHTML = inc;
+	document.getElementById("cs").innerHTML = cst;
 	setTimeout(update,10);
 }
 
 function incrs() {
-if (money > cost) { 
-	money -= cost;
-	increment += 1 + increments ** 1.3;
-	cost = 10 + Math.round(increment ** 1.2)
+if (mn => cst) { 
+	mn -= cst;
+	inc += 1 + increments ** 1.3;
+	cst = 10 + Math.round(inc ** 1.2)
 }
-if (increment => 100) { 
+if (inc => 100) { 
 	document.body.innerHTML = "Bruh you're bored";
 }
 
